a7a8d98600c58cab782aef1118eeb0715e40219e
Hatz <Hatzaknauer@gmail.com>
2022-06-15 00:42:55 -0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -130,7 +130,8 @@ public class Player : MonoBehaviour, IPunObservable
     void Movement()
     {
         Vector2 dir = -transform.right * ver * force;
-        rb.velocity = new Vector2(dir.x, dir.y);
+        //rb.velocity = new Vector2(dir.x, dir.y);
+        rb.AddForce(dir);
 
         float angle = transform.rotation.eulerAngles.z;
         rb.MoveRotation(Quaternion.Euler(0, 0, angle + (-hor * torque)));
